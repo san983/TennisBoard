9cd66f65243da3712cf54e89fcfee4b96cb91ed2
Santiago Traversa <santiago.traversa@gmail.com>
2011-07-07 12:58:35 -0300

    First Commit

---
+++ TennisBoard.Test/FirstTest.cs
@@ -0,0 +1,133 @@
+﻿using NUnit.Framework;
+
+namespace TennisBoard.Test
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        [Test]
+        public void CanCreateNewBoard()
+        {
+            TennisBoard newBoard = new TennisBoard();
+            Assert.IsNotNull(newBoard);
+        }
+
+        [Test]
+        public void WontDisplayBoardResultWithoutPlayerNames()
+        {
+            TennisBoard newBoard = new TennisBoard();
+            Assert.AreNotEqual("Igor: 0 - Olaf: 0", newBoard.GetResult());
+        }
+
+        [Test]
+        public void WillDisplayBoardResultWithGivenPlayerNamesAndScores()
+        {
+            TennisBoard newBoard = new TennisBoard("Damian", "Mati");
+            Assert.AreEqual("Damian: 0 - Mati: 0", newBoard.GetResult());
+        }
+
+        [Test]
+        public void WillTestThatPlayerOneScoresFirstPoint()
+        {
+            TennisBoard newBoard = new TennisBoard("Damian", "Mati");
+            newBoard.PlayerAScores();
+            Assert.AreEqual("Damian: 15 - Mati: 0", newBoard.GetResult());
+        }
+
+        [Test]
+        public void WillTestThatPlayerTwoScoresFirstPoint()
+        {
+            TennisBoard newBoard = new TennisBoard("Damian", "Mati");
+            newBoard.PlayerBScores();
+            Assert.AreEqual("Damian: 0 - Mati: 15", newBoard.GetResult());
+        }
+
+        [Test]
+        public void WillTestBothPlayersScore()
+        {
+            TennisBoard newBoard = new TennisBoard("Damian", "Mati");
+            newBoard.PlayerAScores();
+            newBoard.PlayerBScores();
+            Assert.AreEqual("Damian: 15 - Mati: 15", newBoard.GetResult());
+        }
+
+
+        [Test]
+        public void WillTestScore30_15()
+        {
+            TennisBoard newBoard = new TennisBoard("Damian", "Mati");
+            newBoard.PlayerAScores();
+            newBoard.PlayerAScores();
+            newBoard.PlayerBScores();
+            Assert.AreEqual("Damian: 30 - Mati: 15", newBoard.GetResult());
+        }
+
+        [Test]
+        public void WillTestScore15_30()
+        {
+            TennisBoard newBoard = new TennisBoard("Damian", "Mati");
+            newBoard.PlayerAScores();
+            newBoard.PlayerBScores();
+            newBoard.PlayerBScores();
+            Assert.AreEqual("Damian: 15 - Mati: 30", newBoard.GetResult());
+        }
+
+        [Test]
+        public void WillTestScore40_0()
+        {
+            TennisBoard newBoard = new TennisBoard("Damian", "Mati");
+            newBoard.PlayerAScores();
+            newBoard.PlayerAScores();
+            newBoard.PlayerAScores();
+            Assert.AreEqual("Damian: 40 - Mati: 0", newBoard.GetResult());
+        }
+
+        [Test]
+        public void WillTestPlayerAWinsGame()
+        {
+            TennisBoard newBoard = new TennisBoard("Damian", "Mati");
+            newBoard.PlayerAScores();
+            newBoard.PlayerAScores();
+            newBoard.PlayerAScores();
+            newBoard.PlayerAScores();
+            Assert.AreEqual("Damian Wins Game", newBoard.GetResult());
+        }
+
+        [Test]
+        public void WillTestPlayerBWinsGame()
+        {
+            TennisBoard newBoard = new TennisBoard("Damian", "River");
+            newBoard.PlayerBScores();
+            newBoard.PlayerBScores();
+            newBoard.PlayerBScores();
+            newBoard.PlayerBScores();
+            Assert.AreEqual("River Wins Game", newBoard.GetResult());
+        }
+
+        [Test]
+        public void WillTestPlayersGetToDeuce()
+        {
+            TennisBoard newBoard = new TennisBoard("Damian", "River");
+            newBoard.PlayerAScores();
+            newBoard.PlayerAScores();
+            newBoard.PlayerAScores();
+            newBoard.PlayerBScores();
+            newBoard.PlayerBScores();
+            newBoard.PlayerBScores();
+            Assert.AreEqual("Deuce 40 - 40", newBoard.GetResult());
+        }
+        [Test]
+        public void WillTestPlayerBAdvantage()
+        {
+            TennisBoard newBoard = new TennisBoard("Damian", "River");
+            newBoard.PlayerAScores();
+            newBoard.PlayerAScores();
+            newBoard.PlayerAScores();
+            newBoard.PlayerBScores();
+            newBoard.PlayerBScores();
+            newBoard.PlayerBScores();
+            newBoard.PlayerBScores();
+            Assert.AreEqual("Advantage River", newBoard.GetResult());
+        }
+    }
+}
---
+++ TennisBoard/TennisBoard.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace TennisBoard
+{
+    public class TennisBoard
+    {
+        private string _playerA;
+        private string _playerB;
+        private string _playerAscore;
+        private string _playerBscore;
+
+        public TennisBoard()
+        {
+        }
+
+        public TennisBoard(string playerA, string playerB)
+        {
+            _playerA = playerA;
+            _playerB = playerB;
+            _playerAscore = "0";
+            _playerBscore = "0";
+        }
+
+        public string GetResult()
+        {
+            if ((_playerAscore == "40") && (_playerBscore == "WIN"))
+            {
+                return "Advantage River";
+            }
+            if (_playerAscore == "WIN")
+            {
+                return "Damian Wins Game";
+            }
+            if (_playerBscore == "WIN")
+            {
+                return "River Wins Game";
+            }
+            if ((_playerBscore == "40") && (_playerAscore == "40"))
+            {
+                return "Deuce 40 - 40";
+            }
+           
+
+            return String.Format("{0}: {2} - {1}: {3}", _playerA, _playerB, _playerAscore, _playerBscore);
+        }
+
+        public void PlayerAScores()
+        {
+            _playerAscore = IncrementScore(_playerAscore);
+        }
+
+        public void PlayerBScores()
+        {
+            _playerBscore = IncrementScore(_playerBscore);
+        }
+
+        private static string IncrementScore(string _score)
+        {
+            return _score.Equals("0")
+                ? "15"
+                : _score.Equals("15")
+                    ? "30"
+                    : _score.Equals("30")
+                        ? "40"
+                        : "WIN";
+        }
+
+    }
+}
